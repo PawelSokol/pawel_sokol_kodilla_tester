09c948a9203ee9f9124a3145c5a0f4338dd6d4f2
Pawel Sokol <paveire@wp.pl>
2020-06-14 22:47:06 +0100

    MainExtended

---
+++ kodilla-intro/src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String myName = "Pawel";
+        int myAge = 45;
+        double height = 1.80;
+        char carClass = 'C';
+        boolean isGoodProgrammer = true;
+        System.out.println(myName);
+    }
+}
---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String firstName = "Pawel";
+        String familyName = "Sokol";
+        int yearOfBirth = 1975;
+        char sex = 'M';
+        char catDrivingLicense = 'B';
+        boolean isDrinkingBeer = false;
+        System.out.println(firstName);
+        System.out.println(familyName);
+        System.out.println(yearOfBirth);
+        System.out.println(sex);
+        System.out.println(catDrivingLicense);
+        System.out.println(isDrinkingBeer);
+    }
+}
