55b1bf2702f7326b4fd9502e23261aab8c7fcacc
Pawel Sokol <paveire@wp.pl>
2020-06-14 18:46:14 +0100

    Hello World!

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Pawel");
+        System.out.println(45);
+        System.out.println('S');
+    }
+}
