7ab6c03aef902523e3d70a65a83e0cc7e51418fe
PawelSokol <66559611+PawelSokol@users.noreply.github.com>
2020-07-18 17:22:57 +0100

    Update Notebook.java

--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,9 +1,9 @@
 public class Notebook {
-    String weight;
+    int weight;
     int price;
     int year;
 
-    public Notebook(String weight, int price, int year) {
+    public Notebook(int weight, int price, int year) {
         this.weight = weight;
         this.price = price;
         this.year = year;
@@ -12,7 +12,7 @@ public class Notebook {
     public void checkPrice() {
         if (this.price < 600) {
             System.out.println("This notebook is very cheap.");
-        } else if (this.price > 600 && this.price < 1000){
+        } else if (this.price >= 600 && this.price <= 1000){
             System.out.println("This price is good");
         } else {
             System.out.println("This notebook is expensive");
@@ -21,21 +21,21 @@ public class Notebook {
     public void checkWeight() {
         if (this.weight < 1000) {
             System.out.println("This notebook is light");
-        } else if (this.weight > 1000 && this.weight < 1500) {
+        } else if (this.weight >= 1000 && this.weight <= 1500) {
             System.out.println("This notebook is quite heavy");
         } else {
             System.out.println("This notebook is very heavy");
     }
 
-    public void checkYear() {
-        if (this.year < 2016) {
-            System.out.println("This notebook is old");
-        } else if (this.price < 600 && this.year < 2016){
-            System.out.println("This notebook is a bargain");
-        } else if (this.price < 1100 && this.year <= 2018){
-            System.out.println("This notebook is reasonably priced");
-        } else if (this.price >= 1500);
-            System.out.println("This laptop is overpriced");
+    public void checkOffer() {
+        if (this.year < 2016 && this.price <= 500) {
+            System.out.println("This notebook has normal price");
+        } else if (this.year >= 2016 && this.year < 2019 && this.price > 500 && thi.price <= 1000){
+            System.out.println("This notebook has quite good price");
+        } else if (this.year >= 2019 && this.price > 1000 && this.price <= 1400){
+            System.out.println("This notebook has good price");
+        } else if (this.price >= 1400);
+            System.out.println("This notebook is overpriced");
     }
 
     }
