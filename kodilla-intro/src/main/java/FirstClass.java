39659b5f7474295caac04d9308caa2705471f78f
PawelSokol <66559611+PawelSokol@users.noreply.github.com>
2020-07-18 17:25:08 +0100

    Update FirstClass.java

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -4,18 +4,18 @@ public class FirstClass {
         System.out.println(notebook.weight + "" + notebook.price + "" + notebook.year);
         notebook.checkPrice();
         notebook.checkWeight();
-        notebook.checkYear();
+        notebook.checkOffer();
 
         Notebook heavyNotebook = new Notebook("2000g", 1500, 2020);
         System.out.println(heavyNotebook.weight + "" + heavyNotebook.price + "" + heavy.Notebook.year);
         heavyNotebook.checkPrice();
         heavyNotebook.checkWeight();
-        heavyNotebook.checkYear();
+        heavyNotebook.checkOffer();
 
         Notebook oldNotebook = new Notebook("1200g", 500, 2015);
         System.out.println(oldNotebook.weight + "" + oldNotebook.price + "" + oldNotebook.year);
         oldNotebook.checkPrice();
         oldNotebook.checkWeight();
-        oldNotebook.checkYear();
+        oldNotebook.checkOffer();
     }
 }
